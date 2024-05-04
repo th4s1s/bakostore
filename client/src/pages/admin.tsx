72fc1b09233d5b5c73511870cfff12e342305273
nyankirie <nyankirie@gmail.com>
2024-05-04 14:59:23 +0700

    admin without edit/add

--- client/src/pages/admin.tsx
+++ client/src/pages/admin.tsx
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { ChangeEvent, useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import { DataGrid } from '@mui/x-data-grid';
+// import { useNavigate } from 'react-router-dom';
 
 const Admin = () => {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const userInfor = localStorage.getItem("user");
     const [isAuth, setAuth] = useState(!(!userInfor));
@@ -124,9 +123,10 @@ const Admin = () => {
     const toggleShowMore = (index: string | number) => {
         setShowMore(prevState => ({
           ...prevState,
-          [index]: !prevState[index] // Toggle the state for the specific product ID
+          [index]: !prevState[index]
         }));
       };
+
     const handleShowProduct = async () => {
         if(isAuth){
             try {
@@ -181,9 +181,43 @@ const Admin = () => {
             }
         }
     }
+
+    //news
+    const handleShowNews = async () => {
+        if(isAuth){
+            try {
+                //localhost
+                const response = await axios.get(`/api/admin/news/show.php`);
+                // console.log(response.data)
+                setNewsList(response.data)
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
+    }
+
+    const handleDeleteNews = async (id : string) => {
+        if(isAuth && userInfor){
+            const formData = new URLSearchParams({
+                id: id,
+            });
+            try {
+                //localhost
+                const response = await axios.post(`/api/admin/news/remove.php`, formData, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                });
+                handleShowNews();
+            } catch (error) {
+
+            }
+        }
+    }
     //popup
     const [showDialog, setShowDialog] = useState(false);
     const [showInputForm, setShowInputForm] = useState(false);
+
     const handleShowDialog = (index: any) => {
         setShowDialog((prev) => ({
             ...prev,
@@ -196,12 +230,14 @@ const Admin = () => {
             [index]: false,
         }));
     };
+
     const handleShowInputForm = (index: any) => {
         setShowInputForm((prev) => ({
             ...prev,
             [index]: true,
         }));
     };
+
     const handleHideInputForm = (index: any) => {
         setShowInputForm((prev) => ({
             ...prev,
@@ -233,7 +269,7 @@ const Admin = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        {userList.map((user: { username: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; phone: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; token: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }, index: Key | null | undefined) => (
+                        {userList.map((user, index) => (
                             <tr key={index}>
                             <td className="border border-black px-4 py-2 max-w-[100px]" style={{ wordWrap: 'break-word' }}>{user.username}</td>
                             {/* <td className="border border-black px-4 py-2 overflow-hidden whitespace-nowrap max-w-[400px]">{user.password}</td> */}
@@ -309,7 +345,7 @@ const Admin = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        {productList.map((product: { id: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; description: string | number | boolean | any[] | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; price: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined; type: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; rating: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }, index: Key | null | undefined) => (
+                        {productList.map((product, index) => (
                             <tr key={index}>
                             <td className="border border-black py-2 max-w-[10px]" style={{ wordWrap: 'break-word' }}>{product.id}</td>
                             {/* <td className="border border-black px-4 py-2 overflow-hidden whitespace-nowrap max-w-[400px]">{user.password}</td> */}
@@ -328,7 +364,7 @@ const Admin = () => {
                             <td className="px-4 py-2 max-w-[50px]">
                                 <button
                                     className={`transition ease-in-out delay-50 bg-white hover:-translate-y-0.5 hover:scale-100 hover:bg-pink-400 hover:text-white duration-300 w-14 rounded-lg shadow-lg ${toggleEdit ? 'pointer-events-auto' : 'pointer-events-none opacity-50'}`}
-                                    onClick={() => {handleShowInputForm(index)}}
+                                    onClick={() => {}}
                                 >
                                 Edit
                                 </button>
@@ -361,52 +397,86 @@ const Admin = () => {
                                         </div>
                                     </div>
                                 )}
-                                {showInputForm[index] && (
+                            </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                    </div>
+                    }
+                </>
+            );
+        }
+    }
+
+    const NewsMana = () => {
+        if(isAuth){
+            return (
+                <>
+                    {/* <button onClick={() => {handleShowUser()}}>Click me</button> */}
+                    { newsList &&
+                    <div className="nav bg-pink-100 text-black shadow">
+                    <table className="table-auto w-full text-center">
+                        <thead>
+                        <tr>
+                            <th className="border border-black">Id</th>
+                            {/* <th className="border border-black px-4 py-2">Password</th> */}
+                            <th className="border border-black">Tựa</th>
+                            <th className="border border-black">Nội dung</th>
+                            <th className="border border-black">Date</th>
+                            <th className="max-w-[40px]">
+                                <button className={`transition ease-in-out delay-50 px-4 py-2 rounded-lg shadow-lg duration-300 ${toggleEdit ? 'bg-pink-500 text-white' : 'bg-white hover:-translate-y-1 hover:scale-100 hover:bg-pink-400 hover:text-white'}`}
+                                        onClick={() => {setEdit(!toggleEdit)}}
+                                >
+                                    Bật / tắt chỉnh sửa
+                                </button>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {newsList.map((news, index) => (
+                            <tr key={index}>
+                            <td className="border border-black max-w-[50px]" style={{ wordWrap: 'break-word' }}>{news.id}</td>
+                            {/* <td className="border border-black px-4 py-2 overflow-hidden whitespace-nowrap max-w-[400px]">{user.password}</td> */}
+                            <td className="border border-black px-4 py-2 max-w-[100px]" style={{ wordWrap: 'break-word' }}>{news.title}</td>
+                            <td className="border border-black px-4 py-2 max-w-[400px]" style={{ wordWrap: 'break-word' }}>
+                                {showMore[index] ? news.content : `${news.content.slice(0, 300)}`}
+                                    {news.content.length > 300 && (
+                                    <span className="text-blue-500 cursor-pointer" onClick={() => toggleShowMore(index)}>
+                                        {showMore[index] ? " Thu gọn" : " ...xem thêm"}
+                                    </span>
+                                )}
+                            </td>
+                            <td className="border border-black max-w-[20px]" style={{ wordWrap: 'break-word' }}>{news.date}</td>
+                            <td className="px-4 py-2 max-w-[50px]">
+                                <button
+                                    className={`transition ease-in-out delay-50 bg-white hover:-translate-y-0.5 hover:scale-100 hover:bg-pink-400 hover:text-white duration-300 w-14 rounded-lg shadow-lg ${toggleEdit ? 'pointer-events-auto' : 'pointer-events-none opacity-50'}`}
+                                    onClick={() => {}}
+                                >
+                                Edit
+                                </button>
+                                <button
+                                    className={`transition ease-in-out delay-50 bg-white hover:-translate-y-0.5 hover:scale-100 hover:bg-pink-400 hover:text-white duration-300 w-14 rounded-lg shadow-lg ${toggleEdit ? 'pointer-events-auto' : 'pointer-events-none opacity-50'}`}
+                                    onClick={() => {handleShowDialog(index)}}
+                                >
+                                Xóa
+                                </button>
+                                {showDialog[index] && (
                                     <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-0"
-                                            // onClick={() => handleHideInputForm(index)}
+                                            onClick={() => handleHideDialog(index)}
                                     >
-                                        <div className="bg-white p-4 rounded-lg shadow-lg min-w-[500px]">
-                                            <div className="mb-4">
-                                                <label htmlFor="id" className="block text-sm font-medium text-gray-700">ID</label>
-                                                <input type="text" id="id" name="id" defaultValue={product.id} onBlur={(e) => handleChangeInForm(e)} className="mt-1 p-2 border-gray-300 rounded-md w-full text-center" />
-                                            </div>
-                                            <div className="mb-4">
-                                                <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-                                                <input type="text" id="name" name="name" defaultValue={product.name} onBlur={(e) => handleChangeInForm(e)} className="mt-1 p-2 border-gray-300 rounded-md w-full text-center" />
-                                            </div>
-                                            <div className="mb-4">
-                                                <label htmlFor="description" className="block text-sm font-medium text-gray-700">Description</label>
-                                                <textarea id="description" name="description" defaultValue={product.description} onBlur={(e) => handleChangeInForm(e)} rows="7" className="mt-1 p-2 border-gray-300 rounded-md w-full text-start"></textarea>
-                                            </div>
-                                            <div className="mb-4">
-                                                <label htmlFor="price" className="block text-sm font-medium text-gray-700">Price</label>
-                                                <input type="number" id="price" name="price" defaultValue={product.price} onBlur={(e) => handleChangeInForm(e)} className="mt-1 p-2 border-gray-300 rounded-md w-full text-center" />
-                                            </div>
-                                            <div className="mb-4">
-                                                <label htmlFor="Type" className="block text-sm font-medium text-gray-700">Type</label>
-                                                <input type="text" id="Type" name="Type" defaultValue={product.type} onBlur={(e) => handleChangeInForm(e)} className="mt-1 p-2 border-gray-300 rounded-md w-full text-center" />
-                                            </div>
-                                            <div className="rounded-lg border-2 border-pink-300 p-1 right-0 mb-5 h-10 w-full">
-                                                <input name="fileToUpload" id="fileToUpload" type="file" accept="image/*" className="hidden" onChange={(e) => {handleChangeInFormImg(e)}} />
-                                                Ảnh mới
-                                            </div>
+                                        <div className="bg-white p-4 rounded-lg shadow-lg">
+                                            <p className="text-lg text-gray-800 mb-4">Xóa tin {news.title}</p>
                                             <div className="flex justify-end">
                                                 <button
                                                     className="bg-red-500 hover:bg-red-600 text-white font-semibold px-4 py-2 rounded-lg mr-2"
-                                                    onClick={() => {checkAuth(); handleUpdateProduct(
-                                                        productFormData.id ? productFormData.id : product.id,
-                                                        productFormData.name ? productFormData.name : product.name,
-                                                        productFormData.description ? productFormData.description : product.description,
-                                                        productFormData.price ? productFormData.price : product.price,
-                                                        productFormData.type? productFormData.type : product.type,
-                                                        productFormData.fileToUpload
-                                                    )}}
+                                                    onClick={() => {checkAuth(); handleDeleteNews(news.id)}}
                                                 >
-                                                    Xác nhận
+                                                    Xóa
                                                 </button>
                                                 <button
                                                     className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold px-4 py-2 rounded-lg"
-                                                    onClick={() => handleHideInputForm(index)}
+                                                    onClick={() => handleHideDialog(index)}
                                                 >
                                                     Hủy
                                                 </button>
@@ -425,7 +495,6 @@ const Admin = () => {
             );
         }
     }
-
     const ManaPage = () => {
         return (
         <>
@@ -459,12 +528,13 @@ const Admin = () => {
                     <ul className="flex justify-center gap-40">
                         <li className="mx-2"><a onClick={() => {checkAuth(); handleMana("user"); handleShowUser()} } className={`${isUserMana ? 'underline' : ''} hover:underline`}>Quản lý Người dùng</a></li>
                         <li className="mx-2"><a onClick={() => {checkAuth(); handleMana("product"); handleShowProduct()} } className={`${isProductMana ? 'underline' : ''} hover:underline`}>Quản lý Sản phẩm</a></li>
-                        <li className="mx-2"><a onClick={() => {checkAuth(); handleMana("news"); } } className={`${isNewsMana ? 'underline' : ''} hover:underline`}>Quản lý Tin tức</a></li>
+                        <li className="mx-2"><a onClick={() => {checkAuth(); handleMana("news"); handleShowNews()} } className={`${isNewsMana ? 'underline' : ''} hover:underline`}>Quản lý Tin tức</a></li>
                         <li className="mx-2"><a onClick={() => {checkAuth(); handleMana("return"); } } className={`hover:underline`}>Quay lại</a></li>
                     </ul>
                 </div>
                 {isUserMana && <UserMana />}
                 {isProductMana && <ProductMana />}
+                {isNewsMana && <NewsMana />}
             </>
         );
     }
